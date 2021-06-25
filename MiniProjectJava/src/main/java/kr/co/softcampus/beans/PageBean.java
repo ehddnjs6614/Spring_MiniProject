9517574a484e2487073c1570e135e2e0b1e8bb1d
owner <owner@DESKTOP-PC95CSK>
2021-06-25 23:55:34 +0900

    개인 mini 프로젝트

---
+++ MiniProjectJava/src/main/java/kr/co/softcampus/beans/PageBean.java
@@ -0,0 +1,67 @@
+package kr.co.softcampus.beans;
+
+
+public class PageBean {
+
+	//최소 페이지 번호 
+	private int min;
+	//최대 페이지 번호
+	private int max;
+	//이전 페이지 번호
+	private int prevPage;
+	//다음 페이지 번호
+	private int nextPage;
+	//전체 페이지 번호
+	private int pageCnt;
+	//현재 페이지 번호
+	private int currentPage;
+	
+	
+	//contentCnt : 전체 글개수, currentPage : 현재 글번호 ,contentPageCnt : 페이지당 글의 개수,paginationCnt : 페이지 버튼의 개수
+	public PageBean(int contentCnt, int currentPage, int contentPageCnt, int paginationCnt) {
+		
+		//현재 페이지번호
+		this.currentPage = currentPage;
+		
+		//전체 페이지 개수
+		pageCnt = contentCnt / contentPageCnt;
+		
+		if(contentCnt % contentPageCnt > 0) {
+			pageCnt++;
+		}
+		min = ((currentPage -1) / paginationCnt ) * paginationCnt + 1;
+		max = min + paginationCnt -1;
+		
+		if(max > pageCnt) {
+			max = pageCnt;
+		}
+		
+		prevPage = min - 1;
+		nextPage = max + 1;
+		if(nextPage > pageCnt) {
+			nextPage = pageCnt;
+		}
+	}
+	
+	public int getMin() {
+		return min;
+	}
+	public int getMax() {
+		return max;
+	}
+	public int getPrevPage() {
+		return prevPage;
+	}
+	public int getNextPage() {
+		return nextPage;
+	}
+	public int getPageCnt() {
+		return pageCnt;
+	}
+	public int getCurrentPage() {
+		return currentPage;
+	}
+	
+	
+	
+}
---
+++ MiniProjectJava/src/main/java/kr/co/softcampus/controller/HomeController.java
@@ -0,0 +1,27 @@
+package kr.co.softcampus.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.softcampus.beans.UserBean;
+
+@Controller
+public class HomeController {
+	//로그인세션 주입 테스트
+//	@Resource(name = "loginUserBean")
+//	private UserBean loginUserBean;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(HttpServletRequest request) {
+//		System.out.println(loginUserBean);
+		
+		//System.out.println(request.getServletContext().getRealPath("/"));
+		
+		return "redirect:/main";
+	}
+}
+
